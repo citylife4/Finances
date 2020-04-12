49abe6d0736822c64754815a16b4c12e65f8f032
Beatriz Queiros <boliveiraqueiros@gmail.com>
2020-04-12 16:49:00 +0100

    updated file

--- src/createXML.py
+++ src/createXML.py
@@ -3,37 +3,55 @@ import dicttoxml
 def create_xml(input_filename, output_filename, dataset):
     # Arguments
     black_list = {"ISIN","Country","Currency","Exchange","Quantity","C_Gain_Loss"}
-    rename     = {"Country":"CodPais"}
-    anexos = ["Rosto","AnexoA","AnexoJ"]
-    quadros = ["Quadro01","Quadro02","Quadro03","Quadro04","Quadro05","Quadro06","Quadro07","Quadro08","Quadro09","Quadro10","Quadro11"]
+    annexs = {"Rosto":"","AnexoA":"","AnexoJ" : ["Quadro08","Quadro09"]}
    
-    # Ola
+    #annexs = ["Rosto","AnexoA","AnexoJ"]
+    #tables = ["Quadro01","Quadro02","Quadro03","Quadro04","Quadro05","Quadro06","Quadro07","Quadro08","Quadro09","Quadro10","Quadro11"]
+    
     new_dictiory = {}
-    
      
-    #print(dataset.items())
-    #=new_example = {k:v for k,v in abc.iteritems() if k not in black_list_values}
-    ## se usares um for vais buscar as coisas
+# read dictionary dataset
+    print(dataset.items())
 
-    # ler o dicionario dataset (exemplo)
-    # ir buscar informacao necessaria (parse)
-    new_dataset = {rename.get(key,key) : val for key ,val in dataset.items()}
+# select the needed information from parse
     for key ,val in dataset.items():
+        new_dataset={}
         for k ,v in val.items():
-            new_dataset = {rename.get(kk,kk) : vv for kk ,vv in v.items()}
-            #print(new_dataset)
+            new_dataset_values = {k:v for (k,v) in v.items() if k not in black_list}
+            new_dataset.update({k:[new_dataset_values]})
+        print(new_dataset)
 
-    # criar novo dicionario 
-    # Introduzir informacao como e requerida para o IRS
-    x = 5
-    for anaxo in anexos:
-        new_quadro_line = {}
-        for i in range(0,x) :
-            new_quadro_line.update({"AnexoJq092AT01-Linha_"+str(i) : "asd"})
-        new_dic_line = {anaxo: new_quadro_line}
-        new_dictiory.update(new_dic_line)
-    
+# Create a new dictionary: enter information as required by the IRS
+
+    x = 3   
+    #for i in annexs:
+    for key, value in annexs.items(): 
+        for v in value:
+        #new_table_line = {}
+        #for j in tables:
+            new_line = {}
+            for k in range(1,x) :
+                new_line.update({"AnexoJq092AT01-Linha numero="+str(k):"abc"})
+                new_dic_line = {v: new_line}
+                new_dictiory.update(new_dic_line)
+            #new_dic_table = {i:new_dic_line}
+            #new_dictiory.update(new_dic_table)
     print(new_dictiory)
+
+    IRS = {
+        "NLinha" : "",
+	    "CodPais" : "",
+	    "Codigo" : "",
+	    "AnoRealizacao" : "",
+	    "MesRealizacao" : "",
+	    "ValorRealizacao" : "",
+	    "AnoAquisicao" : "",
+	    "MesAquisicao" : "",
+	    "ValorAquisicao" : "",
+	    "DespesasEncargos" : "",
+	    "ImpostoPagoNoEstrangeiro" : "",
+	    "CodPaisContraparte" : ""
+        }
 
     # Transformar em XML
     xml_snippet = dicttoxml.dicttoxml(new_dataset, root=False)    
@@ -41,13 +59,6 @@ def create_xml(input_filename, output_filename, dataset):
     #Escrever em ficheiro
     with open( "tmp.xml", "w") as f:
         f.write(str(xml_snippet))
-
-
-newExample ={
-    "Ane"
-        "nii"
-        "CodPais"
-}
 
 
 example = {
@@ -84,22 +95,22 @@ example = {
             "Tax" : "0.00",
             "Counterparty_C" : "442"
         },
-            "2" : {
-            "ISIN" : "LU0599900635",
-            "Type" : "G20",
-            "Country" : "442",
-            "Currency" : "EUR",
-            "Exchange" : "1,00000",
-            "Quantity" : "0,02",
-            "A_Date" : "17.06.2019",
-            "A_Value" : "2,50",
-            "S_Date"  : "24.06.2019",
-            "S_Value"  : "2,51",
-            "T_Costs" : "0,00",
-            "C_Gain_Loss" : "0,01",
-            "Tax" : "0.00",
-            "Counterparty_C" : "442"
-        }
+            # "2" : {
+            # "ISIN" : "LU0599900635",
+            # "Type" : "G20",
+            # "Country" : "442",
+            # "Currency" : "EUR",
+            # "Exchange" : "1,00000",
+            # "Quantity" : "0,02",
+            # "A_Date" : "17.06.2019",
+            # "A_Value" : "2,50",
+            # "S_Date"  : "24.06.2019",
+            # "S_Value"  : "2,51",
+            # "T_Costs" : "0,00",
+            # "C_Gain_Loss" : "0,01",
+            # "Tax" : "0.00",
+            # "Counterparty_C" : "442"
+        # }
     }
 }
 create_xml("documents/bla.xml", "output.xml" , example )
