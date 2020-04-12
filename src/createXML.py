01946a38420c430ae4a122eab3af27cd5ba3cc59
Beatriz Queiros <boliveiraqueiros@gmail.com>
2020-04-13 00:41:07 +0100

    new dictionary according IRS

--- src/createXML.py
+++ src/createXML.py
@@ -4,14 +4,11 @@ def create_xml(input_filename, output_filename, dataset):
     # Arguments
     black_list = {"ISIN","Country","Currency","Exchange","Quantity","C_Gain_Loss"}
     annexs = {"Rosto":"","AnexoA":"","AnexoJ" : ["Quadro08","Quadro09"]}
-   
-    #annexs = ["Rosto","AnexoA","AnexoJ"]
-    #tables = ["Quadro01","Quadro02","Quadro03","Quadro04","Quadro05","Quadro06","Quadro07","Quadro08","Quadro09","Quadro10","Quadro11"]
-    
-    new_dictiory = {}
+
+    new_dict = {}
      
 # read dictionary dataset
-    print(dataset.items())
+    #print(dataset.items())
 
 # select the needed information from parse
     for key ,val in dataset.items():
@@ -19,39 +16,32 @@ def create_xml(input_filename, output_filename, dataset):
         for k ,v in val.items():
             new_dataset_values = {k:v for (k,v) in v.items() if k not in black_list}
             new_dataset.update({k:[new_dataset_values]})
-        print(new_dataset)
+        #print(new_dataset)
 
 # Create a new dictionary: enter information as required by the IRS
 
-    x = 3   
-    #for i in annexs:
+    x = int(list(new_dataset.keys())[-1]) + 2
     for key, value in annexs.items(): 
         for v in value:
-        #new_table_line = {}
-        #for j in tables:
             new_line = {}
             for k in range(1,x) :
-                new_line.update({"AnexoJq092AT01-Linha numero="+str(k):"abc"})
+                new_line.update({"AnexoJq092AT01-Linha numero="+str(k):{
+                    "NLinha" : "",
+                    "CodPais" : "",
+                    "Codigo" : "",
+                    "AnoRealizacao" : "",
+                    "MesRealizacao" : "",
+                    "ValorRealizacao" : "",
+                    "AnoAquisicao" : "",
+                    "MesAquisicao" : "",
+                    "ValorAquisicao" : "",
+                    "DespesasEncargos" : "",
+                    "ImpostoPagoNoEstrangeiro" : "",
+                    "CodPaisContraparte" : ""
+                    }})
                 new_dic_line = {v: new_line}
-                new_dictiory.update(new_dic_line)
-            #new_dic_table = {i:new_dic_line}
-            #new_dictiory.update(new_dic_table)
-    print(new_dictiory)
-
-    IRS = {
-        "NLinha" : "",
-	    "CodPais" : "",
-	    "Codigo" : "",
-	    "AnoRealizacao" : "",
-	    "MesRealizacao" : "",
-	    "ValorRealizacao" : "",
-	    "AnoAquisicao" : "",
-	    "MesAquisicao" : "",
-	    "ValorAquisicao" : "",
-	    "DespesasEncargos" : "",
-	    "ImpostoPagoNoEstrangeiro" : "",
-	    "CodPaisContraparte" : ""
-        }
+                new_dict.update({key:new_dic_line})
+    print(new_dict)
 
     # Transformar em XML
     xml_snippet = dicttoxml.dicttoxml(new_dataset, root=False)    
@@ -95,22 +85,22 @@ example = {
             "Tax" : "0.00",
             "Counterparty_C" : "442"
         },
-            # "2" : {
-            # "ISIN" : "LU0599900635",
-            # "Type" : "G20",
-            # "Country" : "442",
-            # "Currency" : "EUR",
-            # "Exchange" : "1,00000",
-            # "Quantity" : "0,02",
-            # "A_Date" : "17.06.2019",
-            # "A_Value" : "2,50",
-            # "S_Date"  : "24.06.2019",
-            # "S_Value"  : "2,51",
-            # "T_Costs" : "0,00",
-            # "C_Gain_Loss" : "0,01",
-            # "Tax" : "0.00",
-            # "Counterparty_C" : "442"
-        # }
+            "2" : {
+            "ISIN" : "LU0599900635",
+            "Type" : "G20",
+            "Country" : "442",
+            "Currency" : "EUR",
+            "Exchange" : "1,00000",
+            "Quantity" : "0,02",
+            "A_Date" : "17.06.2019",
+            "A_Value" : "2,50",
+            "S_Date"  : "24.06.2019",
+            "S_Value"  : "2,51",
+            "T_Costs" : "0,00",
+            "C_Gain_Loss" : "0,01",
+            "Tax" : "0.00",
+            "Counterparty_C" : "442"
+        }
     }
 }
 create_xml("documents/bla.xml", "output.xml" , example )
