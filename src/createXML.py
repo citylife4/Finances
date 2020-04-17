93068e4a69e6793f970739049b5ffdffdf813c73
Beatriz Queiros <boliveiraqueiros@gmail.com>
2020-04-17 23:12:23 +0100

    Clean file

--- src/createXML.py
+++ src/createXML.py
@@ -1,17 +1,10 @@
 import dicttoxml
-from xml.dom.minidom import parseString
 import xml
 import xmltodict
-import json
 
 def create_xml(input_filename, output_filename, dataset):
 # Arguments
     black_list = {"ISIN","Currency","Exchange","Quantity","C_Gain_Loss"}
-     
-
-# read dictionary dataset
-    #print(dataset.items())
-
 
 # select the needed information from parse
     for key ,val in dataset.items():
@@ -19,7 +12,6 @@ def create_xml(input_filename, output_filename, dataset):
         for k ,v in val.items():
             new_dataset_values = {k:v for (k,v) in v.items() if k not in black_list}
             new_dataset.update({k:new_dataset_values})
-
 
 # Create a new dictionary
     x = int(list(new_dataset.keys())[-1]) + 2
@@ -43,73 +35,13 @@ def create_xml(input_filename, output_filename, dataset):
             }
         list1.append(dict1)
 
-
 # Enter information as required by the IRS
     with open(input_filename, "r") as fd:
         irs_dict =xmltodict.parse(fd.read())
     irs_dict['Modelo3IRSv2020']['AnexoJ']['Quadro09']['AnexoJq092AT01']['AnexoJq092AT01-Linha'] = list1
 
-    
-# Transformar em XML
+   
+# Transform to XML and write to a file
     print(xmltodict.unparse(irs_dict, pretty=True))
-
-
-#Escrever em ficheiro  
     with open(output_filename, "w") as outfile:
         outfile.write(xmltodict.unparse(irs_dict, pretty=True))
-
-
-example = {
-    "j_9p2_a": {
-         "0" : {
-            "ISIN" : "LU0599900635",
-            "Type" : "G20",
-            "Country" : "442",
-            "Currency" : "EUR",
-            "Exchange" : "1,00000",
-            "Quantity" : "0,26",
-            "A_Date" : "22.05.2019",
-            "A_Value" : "27,00",
-            "S_Date"  : "24.06.2019",
-            "S_Value"  : "27,18",
-            "T_Costs" : "0,00",
-            "C_Gain_Loss" : "0,18",
-            "Tax" : "0.00",
-            "Counterparty_C" : "442"
-        },
-            "1" : {
-            "ISIN" : "LU0599900635",
-            "Type" : "G20",
-            "Country" : "442",
-            "Currency" : "EUR",
-            "Exchange" : "1,00000",
-            "Quantity" : "0,01",
-            "A_Date" : "17.06.2019",
-            "A_Value" : "0,61",
-            "S_Date"  : "24.06.2019",
-            "S_Value"  : "0,61",
-            "T_Costs" : "0,00",
-            "C_Gain_Loss" : "-0,00",
-            "Tax" : "0.00",
-            "Counterparty_C" : "442"
-        },
-            "2" : {
-            "ISIN" : "LU0599900635",
-            "Type" : "G20",
-            "Country" : "442",
-            "Currency" : "EUR",
-            "Exchange" : "1,00000",
-            "Quantity" : "0,02",
-            "A_Date" : "17.06.2019",
-            "A_Value" : "2,50",
-            "S_Date"  : "24.06.2019",
-            "S_Value"  : "2,51",
-            "T_Costs" : "0,00",
-            "C_Gain_Loss" : "0,01",
-            "Tax" : "0.00",
-            "Counterparty_C" : "442"
-        }
-    }
-}
-
-#create_xml("input.xml", "irs.xml" , example )
